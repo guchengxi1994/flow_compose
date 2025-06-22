304ce21569fc5156acee4f6a24228f799bb73b99
xiaoshuyui <528490652@qq.com>
2025-06-22 18:21:01 +0800

    Update expanable_widget.dart

--- lib/src/nodes/expanable_widget.dart
+++ lib/src/nodes/expanable_widget.dart
@@ -32,7 +32,7 @@ class _ExpanableWidgetState extends State<ExpanableWidget> {
         padding: isExpanded ? EdgeInsets.all(20) : EdgeInsets.all(5),
         child: AnimatedContainer(
           width: isExpanded ? 240 : 30,
-          height: isExpanded ? MediaQuery.of(context).size.height - 100 : 30,
+          height: isExpanded ? MediaQuery.of(context).size.height * 0.7 : 30,
           decoration: BoxDecoration(
             borderRadius: isExpanded
                 ? BorderRadius.circular(20)
